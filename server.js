2226028d664da2a329aaefc9124fbbd6eae65a12
fikri28300 <f_pekdemir28@outlook.com>
2026-01-14 10:12:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,27 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// HTML, CSS, JS dosyalarını aç
+// Form verilerini okumak için
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Statik dosyalar (html, css)
 app.use(express.static(__dirname));
 
-// Ana adresi login.html'e yönlendir
+// Ana sayfa → login
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "login.html"));
+});
+
+// LOGIN KONTROLÜ
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (username === "admin" && password === "1234") {
+    res.redirect("/tasks.html");
+  } else {
+    res.send("Hatalı kullanıcı adı veya şifre");
+  }
 });
 
 app.listen(PORT, () => {
